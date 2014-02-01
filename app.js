4c0d4a0e5ce1765e201201e8bdfcc35fc91c610c
HiroshiOyamada <h5y1m141@gmail.com>
2014-02-02 08:44:36 +0900

    enable to show google map

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 (function() {
-  var app, conf, express, http, path, routes, user;
+  var app, conf, express, http, path, routes;
 
   express = require('express');
 
   routes = require('./routes');
-
-  user = require('./routes/user');
 
   http = require('http');
 
@@ -15,17 +13,15 @@
 
   app = express();
 
-  console.log(conf.apiKey.development);
-
-  console.log(conf.user.id);
-
   app.set('port', process.env.PORT || 3000);
 
   app.set('views', path.join(__dirname, 'views'));
 
   app.set('view engine', 'jade');
 
-  app.use(express.favicon());
+  app.use(express["static"](path.join(__dirname, "public")));
+
+  app.use(express.favicon(__dirname + "/public/images/favicon.ico"));
 
   app.use(express.logger('dev'));
 
@@ -37,15 +33,11 @@
 
   app.use(app.router);
 
-  app.use(express["static"](path.join(__dirname, 'public')));
-
   if (app.get('env') === 'development') {
     app.use(express.errorHandler());
   }
 
   app.get('/', routes.index);
-
-  app.get('/users', user.list);
 
   http.createServer(app).listen(app.get('port'), function() {
     return console.log("Express server listening on port " + (app.get('port')));
--- coffee/app.coffee
+++ coffee/app.coffee
@@ -1,24 +1,25 @@
 express = require('express')
 routes = require('./routes')
-user = require('./routes/user')
+
 http = require('http')
 path = require('path')
 conf = require('config')
 app = express()
 
-console.log conf.apiKey.development
-console.log conf.user.id
 
 app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
-app.use(express.favicon())
+
+app.use express.static(path.join(__dirname, "public"))
+app.use express.favicon(__dirname + "/public/images/favicon.ico")
+
 app.use(express.logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.methodOverride())
 app.use(app.router)
-app.use(express.static(path.join(__dirname, 'public')))
+
 
 
 if app.get('env') is 'development'
@@ -26,7 +27,6 @@ if app.get('env') is 'development'
 
 
 app.get('/', routes.index)
-app.get('/users', user.list)
 
 http.createServer(app).listen(app.get('port'), ->
   console.log "Express server listening on port #{app.get('port')}"
