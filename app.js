041571b5fe6e3fdecdc7770e2d3e3827f9f21e99
HiroshiOyamada <h5y1m141@gmail.com>
2014-02-02 07:42:35 +0900

    setup coffeescript env

--- app.js
+++ app.js
@@ -1,38 +1,54 @@
+(function() {
+  var app, conf, express, http, path, routes, user;
 
-/**
- * Module dependencies.
- */
+  express = require('express');
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-var conf = require('config');
-var app = express();
+  routes = require('./routes');
 
-console.log(conf.apiKey.development);
+  user = require('./routes/user');
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+  http = require('http');
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+  path = require('path');
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+  conf = require('config');
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+  app = express();
+
+  console.log(conf.apiKey.development);
+
+  console.log(conf.user.id);
+
+  app.set('port', process.env.PORT || 3000);
+
+  app.set('views', path.join(__dirname, 'views'));
+
+  app.set('view engine', 'jade');
+
+  app.use(express.favicon());
+
+  app.use(express.logger('dev'));
+
+  app.use(express.json());
+
+  app.use(express.urlencoded());
+
+  app.use(express.methodOverride());
+
+  app.use(app.router);
+
+  app.use(express["static"](path.join(__dirname, 'public')));
+
+  if (app.get('env') === 'development') {
+    app.use(express.errorHandler());
+  }
+
+  app.get('/', routes.index);
+
+  app.get('/users', user.list);
+
+  http.createServer(app).listen(app.get('port'), function() {
+    return console.log("Express server listening on port " + (app.get('port')));
+  });
+
+}).call(this);
